88f338956773ab994c05cc7e40cecb6862ba810d
LilithCode <barbieatonline@gmail.com>
2025-02-08 16:29:21 +0500

    Updated script.js with new changes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let isPlaying = false;
 async function fetchSpotifyData() {
   try {
     const response = await fetch(
-      `http://127.0.0.1:3002/Spotify/songs/${currentFolder}/`
+      `http://127.0.0.1:3000/Spotify/songs/${currentFolder}/`
     );
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.statusText}`);
